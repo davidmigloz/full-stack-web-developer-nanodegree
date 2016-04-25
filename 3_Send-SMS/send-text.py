21495f2603cc1e33815a29bdbc006992822f534c
davidmigloz <me@davidmiguel.com>
2016-04-26 00:35:34 +0200

    add send sms

--- 3_Send-SMS/send-text.py
+++ 3_Send-SMS/send-text.py
@@ -2,7 +2,7 @@ from twilio.rest import TwilioRestClient
  
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "AC130c01770077cef69ef5bc6d8b9a8477"
-auth_token  = "fcbc0a3c98c5790178fc32c912db832a"
+auth_token  = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(body="Alba please?! I love you <3",
